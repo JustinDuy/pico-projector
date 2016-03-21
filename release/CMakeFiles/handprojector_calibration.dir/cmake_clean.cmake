3da432a7973e86ab472a61c378de6a731439d147
duy <judichi87@gmail.com>
2016-03-21 17:45:31 +0100

    add : known camera-base and scale factor

---
+++ release/CMakeFiles/handprojector_calibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/handprojector_calibration.dir/src/graycode.cpp.o"
+  "libhandprojector_calibration.pdb"
+  "libhandprojector_calibration.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/handprojector_calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ release/CMakeFiles/handprojector_calibration_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/handprojector_calibration_main.dir/src/main.cpp.o"
+  "handprojector_calibration_main.pdb"
+  "handprojector_calibration_main"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/handprojector_calibration_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
